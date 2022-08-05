280f41e09a251a77927c6455b2e2b3cc62443ba3
unknown <maximilian.gerhardt@rub.de>
2022-08-05 12:47:04 +0200

    First successful build

--- Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/tools/platformio/platformio-build.py
+++ Arduino15/packages/SPRESENSE/hardware/spresense/1.0.0/tools/platformio/platformio-build.py
@@ -33,10 +33,74 @@ board_config = env.BoardConfig()
 board_name = env.subst("$BOARD")
 
 FRAMEWORK_DIR = platform.get_package_dir("framework-arduinosonyspresense")
+SDK_DIR = platform.get_package_dir("tool-arduinosonyspresensesdk")
 assert isdir(FRAMEWORK_DIR)
+assert isdir(SDK_DIR)
+
+env.ProcessFlags(board_config.get("build.framework_extra_flags.arduino", ""))
+
+#
+# Process configuration flags
+#
+
+cpp_defines = env.Flatten(env.get("CPPDEFINES", []))
+
+# default settings
+debug_enabled = False
+target_core = ("main", None)
+libraries = list()
+
+if "PIO_FRAMEWORK_ARDUINO_ENABLE_DEBUG" in cpp_defines:
+    debug_enabled = True
+    env.Append(CPPDEFINES=["BRD_DEBUG"])
+if "PIO_FRAMEWORK_ARDUINO_CORE_MAIN_CORE" in cpp_defines:
+    pass
+if "PIO_FRAMEWORK_ARDUINO_CORE_SUB_CORE_1" in cpp_defines:
+    target_core = ("sub", 1)
+    env.Append(CPPDEFINES=[("SUBCORE", 1)])
+    libs
+if "PIO_FRAMEWORK_ARDUINO_CORE_SUB_CORE_2" in cpp_defines:
+    target_core = ("sub", 2)
+    env.Append(CPPDEFINES=[("SUBCORE", 2)])
+if "PIO_FRAMEWORK_ARDUINO_CORE_SUB_CORE_3" in cpp_defines:
+    target_core = ("sub", 3)
+    env.Append(CPPDEFINES=[("SUBCORE", 3)])
+if "PIO_FRAMEWORK_ARDUINO_CORE_SUB_CORE_4" in cpp_defines:
+    target_core = ("sub", 4)
+    env.Append(CPPDEFINES=[("SUBCORE", 4)])
+if "PIO_FRAMEWORK_ARDUINO_CORE_SUB_CORE_5" in cpp_defines:
+    target_core = ("sub", 5)
+    env.Append(CPPDEFINES=[("SUBCORE", 5)])
+
+if target_core[0] == "main":
+    libraries = [
+        "libapps.a", "libarch.a", "libarm_cortexM4lf_math.a", "libaudio.a", "libbinfmt.a", "libboard.a", "libboards.a", 
+        "libc.a", "libcmsis_nn.a", "libdrivers.a", "libfs.a", "libmm.a", "libnet.a", "libnnablart.a", "libsched.a", 
+        "libsslutils.a", "libxx.a"
+    ]
+else:
+    libraries = [
+        "libapps.a", "libarch.a", "libarm_cortexM4lf_math.a", "libbinfmt.a", "libboard.a", "libboards.a", "libc.a", 
+        "libcmsis_nn.a", "libdrivers.a", "libfs.a", "libmm.a", "libsched.a", "libxx.a"
+    ]
+
+build_type_prefix = "" if target_core[0] == "main" else "subcore-"
+build_type = build_type_prefix + "release" if not debug_enabled else "debug"
+kernel_path = join(SDK_DIR, build_type)
+libpath = join(kernel_path, "nuttx", "libs")
+
+original_variant = board_config.get("build.arduino.variant", "spresense")
+# subcores use a different variant
+variant = original_variant if target_core[0] == "main" else "spresense_sub"
+
+computed_libs = [File(join(libpath, l)) for l in libraries]
+computed_libs.extend([
+    "gcc",
+    "m",
+    "supc++_nano"
+])
 
 mcu = board_config.get("build.mcu", "")
-mcu_type = mcu[:-2]
 variant = board_config.get(
     "build.variant", board_config.get("build.arduino.variant", "spresense"))
 
@@ -71,6 +135,7 @@ env.Append(
         "-fno-rtti",
         "-fno-exceptions",
         "-fno-use-cxa-atexit",
+        "-fpermissive",
     ],
     CCFLAGS=[
         "-Os",  # optimize for size
@@ -84,7 +149,6 @@ env.Append(
         "-mthumb",
         "-ffunction-sections",  # place each function in its own section
         "-fdata-sections",
-        "-fpermissive",
         "-fno-builtin",
         "-fno-strict-aliasing",
         "-fno-strength-reduce",
@@ -100,61 +164,44 @@ env.Append(
         "ARDUINO_ARCH_SPRESENSE",
         "ARDUINO_%s" % board_id,
         ("BOARD_NAME", '\\"%s\\"' % board_id),
+        "CONFIG_WCHAR_BUILTIN",
+        "CONFIG_HAVE_DOUBLE",
+        "__NuttX__"
     ],
     CPPPATH=[
-        join(FRAMEWORK_DIR, "cores", "arduino", "api", "deprecated"),
+        join(FRAMEWORK_DIR, "cores", "spresense"),
+        join(FRAMEWORK_DIR, "cores", "spresense", "avr"),
+        join(kernel_path, "nuttx", "include", "libcxx"),
+        join(kernel_path, "nuttx", "include"),
+        join(kernel_path, "nuttx", "arch"),
+        join(kernel_path, "nuttx", "arch", "chip"),
+        join(kernel_path, "sdk", "include"),
+        join(kernel_path, "sdk", "modules", "include"),
+        join(kernel_path, "sdk", "apps", "include"),
+        join(kernel_path, "sdk", "system", "include"),
+        join(kernel_path, "sdk", "externals", "include"),
+        join(kernel_path, "sdk", "externals", "include", "cmsis"),
     ],
     LINKFLAGS=[
-        "-ggdb"
+        "-ggdb",
         "-mthumb",
         "-mcpu=%s" % board_config.get("build.cpu"),
         "-mfloat-abi=hard",
-        "-mfpu=fpv4-sp-d16"
+        "-mfpu=fpv4-sp-d16",
         "-Xlinker",
-        "--entry=__start"
-        "-nodefaultlibs"
+        "--entry=__start",
+        "-nodefaultlibs",
         "-nostartfiles",
-        "-Wl,--defsym,__reserved_ramsize=1572864-786432",
+        "-Wl,--defsym,__reserved_ramsize=1572864-%d" % board_config.get("upload.maximum_size"),
         # linkerscript
         "-Wl,--gc-sections",
         '-Wl,-Map="%s"' % join("${BUILD_DIR}", "${PROGNAME}.map"),
         "-u spresense_main"
     ],
-    LIBS=[
-        # All freaking libs
-        "gcc",
-        "m",
-        "supc++_nano",
-    ],
+    LIBS=computed_libs,
 #    LIBPATH=[join(CMSIS_DIR, "DSP", "Lib", "GCC")],
 )
 
-env.ProcessFlags(board_config.get("build.framework_extra_flags.arduino", ""))
-
-#
-# Linker requires preprocessing with correct RAM|ROM sizes
-#
-
-if not board_config.get("build.ldscript", ""):
-    if not isfile(join(env.subst(variant_dir), "ramconfig.ld")):
-        print("Warning! Cannot find linker script for the current target!\n")
-    env.Append(
-        LINKFLAGS=[
-            (
-                "-Wl,--default-script",
-                join(
-                    inc_variant_dir,
-                    board_config.get("build.arduino.ldscript", "ramconfig.ld"),
-                ),
-            )
-        ]
-    )
-
-#
-# Process configuration flags
-#
-
-cpp_defines = env.Flatten(env.get("CPPDEFINES", []))
 
 # copy CCFLAGS to ASFLAGS (-x assembler-with-cpp mode)
 env.Append(ASFLAGS=env.get("CCFLAGS", [])[:])
